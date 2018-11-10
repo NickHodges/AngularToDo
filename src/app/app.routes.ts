ad5ec5cd861d0f3e279e9dfa3ad63dd997c2a8be
Nick Hodges <nickhodges@gmail.com>
2018-11-10 15:52:03 -0500

    Added Routes module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { AboutComponent } from './about/about.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { AllTasksComponent } from './all-tasks/all-tasks.component';
 import { Routes } from '@angular/router';
+import { Component } from '@angular/core';
 
 export const routes: Routes = [
   {
@@ -15,5 +16,9 @@ export const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent
+  },
+  {
+    path: '**',
+    component: TodoListComponent
   }
 ];
