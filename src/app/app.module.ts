b57bf73f40bd26087df9d37f7dad2d5be75c92e9
Nick Hodges <nickhodges@gmail.com>
2018-12-12 10:00:15 -0500

    Fixing up Step2.  Somehow Step4 ended up in here.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
-import { NavbarComponent } from './navbar/navbar.component';
 import { AppComponent } from './app.component';
-import { routes } from './app.routes';
-import { AllTasksComponent } from './all-tasks/all-tasks.component';
-import { AboutComponent } from './about/about.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // Added in Step 3
-    AboutComponent,
-    TodoListComponent,
-    NavbarComponent,
-    // Added in Step 4
-    AllTasksComponent
   ],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
