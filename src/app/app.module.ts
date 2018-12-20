05d0504551c7111958aa51361db0f949265caa79
Nick Hodges <nickhodges@gmail.com>
2018-12-20 10:12:14 -0500

    Adding components back in.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+// Added in Step 4
+import { AboutComponent } from './about/about.component';
+import { AllTasksComponent } from './all-tasks/all-tasks.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     // Added in Step 3
-    NavbarComponent
+    NavbarComponent,
+    // Added in Step 4
+    AboutComponent,
+    AllTasksComponent,
+    TodoListComponent
   ],
   imports: [BrowserModule],
   providers: [],
