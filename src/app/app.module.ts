e3b5ea481597cc7c89cae79682858bc29a8e2c09
Nick Hodges <nickhodges@gmail.com>
2018-12-21 20:39:07 -0500

    Trying to get things back to normal #10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-// Added in Step 4
-import { AboutComponent } from './about/about.component';
-import { AllTasksComponent } from './all-tasks/all-tasks.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
-import { routes } from './app.routes';
 import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { AppComponent } from './app.component';
+import { routes } from './app.routes';
+import { AllTasksComponent } from './all-tasks/all-tasks.component';
+import { AboutComponent } from './about/about.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoDataService } from './todo-data.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     // Added in Step 3
+    AboutComponent,
+    TodoListComponent,
     NavbarComponent,
     // Added in Step 4
-    AboutComponent,
-    AllTasksComponent,
-    TodoListComponent
+    AllTasksComponent
   ],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
-  providers: [],
+  imports: [
+    BrowserModule,
+    // Added for Step 5
+    RouterModule.forRoot(routes),
+    HttpModule,
+    HttpClientModule
+  ],
+  // Added for Step 5
+  providers: [TodoDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
