a7e5641195e484dfce37812927fcd38b9bd08a74
Nick Hodges <nickhodges@gmail.com>
2019-06-23 16:13:16 -0400

    Added subscription storage and unsubscribe

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,31 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Todo } from '../todo';
 import { TodoDataService } from '../todo-data.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
-export class TodoListComponent implements OnInit {
-  constructor(private todoDataService: TodoDataService) { }
+export class TodoListComponent implements OnInit, OnDestroy {
+  constructor(private todoDataService: TodoDataService) {}
 
   // Everything below is added for Step 7
-  completetodos: Array<Todo>;
-  incompletetodos: Array<Todo>;
+  private completetodos: Array<Todo>;
+  private incompletetodos: Array<Todo>;
+  private completeSubscription: Subscription;
+  private incompleteSubscription: Subscription;
 
   ngOnInit() {
     this.RefreshTodos();
   }
 
+  ngOnDestroy(): void {
+    this.completeSubscription.unsubscribe();
+    this.incompleteSubscription.unsubscribe();
+  }
+
   completedTodos() {
-    this.todoDataService
+    this.completeSubscription = this.todoDataService
       .completedTodos()
       .subscribe(todos => (this.completetodos = todos));
   }
 
   incompletedToDos() {
-    this.todoDataService
+    this.incompleteSubscription = this.todoDataService
       .incompletedTodos()
       .subscribe(todos => (this.incompletetodos = todos));
   }
@@ -34,5 +42,4 @@ export class TodoListComponent implements OnInit {
     this.completedTodos();
     this.incompletedToDos();
   }
-
 }
