10b071b71eb6fe28236b1418b877c726f829bb54
Nick Hodges <nickhodges@gmail.com>
2019-06-26 18:58:53 -0400

    Adding unsubscribe logic

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -14,28 +14,30 @@ export class TodoListComponent implements OnInit, OnDestroy {
   // Everything below is added for Step 7
   private completetodos: Array<Todo>;
   private incompletetodos: Array<Todo>;
-  private completeSubscription: Subscription;
-  private incompleteSubscription: Subscription;
+  private subscriptions: Subscription = new Subscription();
 
   ngOnInit() {
     this.RefreshTodos();
   }
 
   ngOnDestroy(): void {
-    this.completeSubscription.unsubscribe();
-    this.incompleteSubscription.unsubscribe();
+    this.subscriptions.unsubscribe();
   }
 
   completedTodos() {
-    this.completeSubscription = this.todoDataService
-      .completedTodos()
-      .subscribe(todos => (this.completetodos = todos));
+    this.subscriptions.add(
+      this.todoDataService
+        .completedTodos()
+        .subscribe(todos => (this.completetodos = todos))
+    );
   }
 
   incompletedToDos() {
-    this.incompleteSubscription = this.todoDataService
-      .incompletedTodos()
-      .subscribe(todos => (this.incompletetodos = todos));
+    this.subscriptions.add(
+      this.todoDataService
+        .incompletedTodos()
+        .subscribe(todos => (this.incompletetodos = todos))
+    );
   }
 
   private RefreshTodos() {
