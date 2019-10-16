f7575b81e78970908490828d0b42d3efba8dfcc1
Nick Hodges <nickhodges@gmail.com>
2019-10-15 21:03:07 -0400

    Remove duplicate identifiers.

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
 import { Todo } from '../todo';
 import { TodoDataService } from '../todo-data.service';
 import { Subscription } from 'rxjs';
