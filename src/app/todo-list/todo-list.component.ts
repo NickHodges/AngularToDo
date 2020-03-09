1be69d3247bbbc62186914b527d8492449cb4769
Nick Hodges <nickhodges@gmail.com>
2020-03-09 18:10:56 -0400

    Updating index name

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -70,8 +70,8 @@ export class TodoListComponent implements OnInit, OnDestroy {
   // Added for Step 11
   removeTodo(todo: Todo) {
     this.subscriptions.add(
-      this.todoDataService.deleteTodoById(todo.todoid).subscribe(val => {
-        const index = this.incompletetodos.findIndex(thetodo => thetodo.todoid === todo.todoid);
+      this.todoDataService.deleteTodoById(todo.id).subscribe(val => {
+        const index = this.incompletetodos.findIndex(thetodo => thetodo.id === todo.id);
         this.incompletetodos.splice(index, 1);
       })
     );
@@ -87,14 +87,10 @@ export class TodoListComponent implements OnInit, OnDestroy {
     todo.title = editInput.value;
     todo.editMode = false;
     this.subscriptions.add(
-      this.todoDataService.updateTodoById(todo.todoid, todo).subscribe(val => {
-        const index = this.incompletetodos.findIndex(thetodo => thetodo.todoid === val.todoid);
+      this.todoDataService.updateTodoById(todo.id, todo).subscribe(val => {
+        const index = this.incompletetodos.findIndex(thetodo => thetodo.id === val.id);
         this.incompletetodos[index] = todo;
       })
     );
-    this.todoDataService.deleteTodoById(todo.id).subscribe(val => {
-      const index = this.incompletetodos.findIndex(thetodo => thetodo.id === todo.id);
-      this.incompletetodos.splice(index, 1);
-    });
   }
 }
