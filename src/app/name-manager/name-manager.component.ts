3f1679e9709fbcaceae146a6636240c1fa8afefa
Nick Hodges <nickhodges@gmail.com>
2020-03-31 16:53:22 -0400

    Better variable name

--- src/app/name-manager/name-manager.component.ts
+++ src/app/name-manager/name-manager.component.ts
@@ -1,6 +1,8 @@
 // This component was added for Step 21
 
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store, select } from '@ngrx/store';
 
 @Component({
   selector: 'app-name-manager',
@@ -8,13 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./name-manager.component.css']
 })
 export class NameManagerComponent implements OnInit {
-  public showMe: boolean = true;
+  public showNameGetter: boolean;
+  name$: Observable<string>;
+  tempName: string;
 
-  constructor() {}
+  constructor(private store: Store<{ name: string }>) {
+    this.name$ = this.store.pipe(select('name'));
+    this.name$.subscribe(theName => (this.tempName = theName));
+    this.showNameGetter = this.tempName == '';
+  }
 
   ngOnInit(): void {}
 
   toggle() {
-    this.showMe = !this.showMe;
+    this.showNameGetter = !this.showNameGetter;
   }
 }
