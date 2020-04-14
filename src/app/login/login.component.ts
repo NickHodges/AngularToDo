14e38489d615a14d3857853cd4607f7f61fe00c8
Nick Hodges <nickhodges@gmail.com>
2020-04-13 23:02:12 -0400

    Getting error messages for bad login working.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,6 +11,8 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
 
+  errorMessage: string;
+
   constructor(private fb: FormBuilder, private authService: AuthenticationService, private router: Router, private route: ActivatedRoute) {
     this.loginForm = this.fb.group({
       email: ['', Validators.required],
@@ -24,9 +26,15 @@ export class LoginComponent implements OnInit {
     const val = this.loginForm.value;
 
     if (val.password && val.email) {
-      this.authService.login(val.email, val.password).subscribe(() => {
-        this.router.navigate(['../todos'], { relativeTo: this.route });
-      });
+      this.authService.login(val.email, val.password).subscribe(
+        () => {
+          this.router.navigate(['../todos'], { relativeTo: this.route });
+        },
+        response => {
+          this.errorMessage = response.error.message;
+          console.log('errorMessage: ', this.errorMessage);
+        }
+      );
     }
   }
 }
