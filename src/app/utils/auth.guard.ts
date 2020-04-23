f0db5dd3d5cdc5308551128ef2b2e9e01a882ff4
Nick Hodges <nickhodges@gmail.com>
2020-04-23 14:55:11 -0400

    Much simpler authguard

--- src/app/utils/auth.guard.ts
+++ src/app/utils/auth.guard.ts
@@ -1,29 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, CanLoad, Route } from '@angular/router';
 import { AuthenticationService } from './authentication.service';
-import { Observable } from 'rxjs';
-import { take, defaultIfEmpty } from 'rxjs/operators';
+
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuardService implements CanActivate {
   constructor(private router: Router, private authenticationService: AuthenticationService) {}
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    const isLoggedIn$: Observable<boolean> = this.authenticationService.isLoggedIn;
-    let result: Boolean = false;
-    isLoggedIn$
-      .pipe(
-        defaultIfEmpty(false),
-        take(1)
-      )
-      .subscribe((loggedIn: boolean) => {
-        result = loggedIn;
-      });
-
-    if (result) {
-      return true;
-    }
-    this.router.navigate(['/sorry'], { queryParams: { returnUrl: state.url } });
-    return false;
+  canActivate() {
+    return this.authenticationService.isLoggedIn;
   }
 }
