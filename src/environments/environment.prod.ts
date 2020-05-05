345383879eb87fdb64a3365ee7d056c2a1757dc4
Nick Hodges <nickhodges@gmail.com>
2020-05-05 14:15:16 -0400

    Adding production information

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  auth: {
+    clientID: 'YCprbyw8Ky75qishq2L10GjnosLRdaC7',
+    domain: 'a4rb.auth0.com',
+    audience: 'https://localhost:3000',
+    redirect: 'https://localhost:4200/callback',
+    scope: 'openid profile email'
+  }
 };
