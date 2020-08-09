42f1c731264c4be356a74310d5eff781b341d98d
Nick Hodges <nickhodges@gmail.com>
2020-08-09 11:24:01 -0400

    Don't do Step7 in Step6

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,48 +1,12 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Todo } from '../todo';
-import { TodoDataService } from '../todo-data.service';
-import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
-export class TodoListComponent implements OnInit, OnDestroy {
-  constructor(private todoDataService: TodoDataService) {}
+export class TodoListComponent implements OnInit {
+  constructor() {}
 
-  // Everything below is added for Step 7
-  private subscriptions: Subscription = new Subscription();
-  public completetodos: Array<Todo>;
-  public incompletetodos: Array<Todo>;
-
-
-  ngOnInit() {
-    this.RefreshTodos();
-  }
-
-  ngOnDestroy(): void {
-    this.subscriptions.unsubscribe();
-  }
-
-  completedTodos() {
-    this.subscriptions.add(
-      this.todoDataService
-        .completedTodos()
-        .subscribe(todos => (this.completetodos = todos))
-    );
-  }
-
-  incompletedToDos() {
-    this.subscriptions.add(
-      this.todoDataService
-        .incompletedTodos()
-        .subscribe(todos => (this.incompletetodos = todos))
-    );
-  }
-
-  private RefreshTodos() {
-    this.completedTodos();
-    this.incompletedToDos();
-  }
+  ngOnInit() {}
 }
